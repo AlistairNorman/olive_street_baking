4cd8d70ff278645342437c76663edfc78909b59f
Alistair Norman <alistair@super.gd>
2024-11-02 21:06:53 -0700

    Run stimulus install

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,6 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@stripe/stripe-js", to: "https://ga.jspm.io/npm:@stripe/stripe-js@2.4.0/dist/stripe.esm.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"

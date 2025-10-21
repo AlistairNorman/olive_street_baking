eafe568609181df6197b4949b64c43621a129dd6
Alistair Norman <alistair@super.gd>
2025-10-21 01:54:00 -0700

    Fix some things

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,7 @@ class HomeController < StoreController
     # Split products into groups of 3 for the homepage blocks.
     # You probably want to remove this logic and use your own!
     homepage_groups = @products.in_groups_of(3, false)
-    @featured_products = homepage_groups[0]
+    @featured_products = [Spree::Product.sole]
     @collection_products = homepage_groups[1]
     @cta_collection_products = homepage_groups[2]
     @new_arrivals = homepage_groups[3]

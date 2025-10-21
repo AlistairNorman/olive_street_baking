becf1f6dd6ebbafcf6dfde3fc0deffd52f668ae5
Alistair Norman <alistair@super.gd>
2025-10-21 01:11:16 -0700

    Fix tests
    Devise issue with Rails 8

--- spec/support/solidus_starter_frontend/database_cleaner.rb
+++ spec/support/solidus_starter_frontend/database_cleaner.rb
@@ -1,3 +1,11 @@
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
+
+  # TODO Remove when Devise fixes https://github.com/heartcombo/devise/issues/5705
+  config.before(:each, type: :controller) do
+    Rails.application.reload_routes_unless_loaded
+  end
+  config.before(:each, type: :request) do
+    Rails.application.reload_routes_unless_loaded
+  end
 end

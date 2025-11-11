8cfd95a12f22a64a4d6c0f1ebd94af1218775074
Alistair Norman <alistair@super.gd>
2025-11-11 15:22:38 -0800

    Make Allie an order automatically

---
+++ spec/controllers/spree/api/stock_items_controller_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'solidus_starter_frontend_spec_helper'
+
+RSpec.describe Spree::Api::StockItemsController, type: :request do
+  describe 'PATCH #update' do
+    subject {
+      put path,
+        params: {
+          stock_item: {
+            count_on_hand: 4
+          }
+        }
+    }
+
+    let(:path) {
+      spree.api_stock_location_stock_item_path(
+        stock_location, stock_item
+      )
+    }
+    let!(:variant) { create(:variant) }
+    let!(:stock_location) { variant.stock_locations.first }
+    let(:stock_item) { variant.stock_items.first }
+    let(:allie) { create :user, email: ENV['ALLIE_EMAIL'] }
+    let(:current_api_user) { create :admin_user }
+
+    before do
+      create :completed_order_with_totals,
+        line_items_attributes: [{ variant: create(:variant), quantity: 1 }],
+        user: allie
+
+      allow(Spree::User).to receive(:find_by).with(email: ENV['ALLIE_EMAIL']) { allie }
+      allow(Spree.user_class)
+        .to receive(:find_by)
+        .with(hash_including(:spree_api_key)) { current_api_user }
+    end
+
+    context "when the variant already has an order for Allie" do
+      before do
+        create :completed_order_with_totals,
+          line_items_attributes: [{ variant: variant, quantity: 1 }],
+          user: allie
+      end
+
+      it "doesn't create a new order for her" do
+        expect { subject }
+          .not_to change { Spree::Order.count }
+          .from(2)
+      end
+    end
+
+    context "when the variant does not have any orders for Allie" do
+      it "creates a new order for her" do
+        expect { subject }
+          .to change { Spree::Order.complete.count }
+          .from(1)
+          .to(2)
+
+        expect(Spree::Order.last.user).to eq(allie)
+        expect(Spree::Order.last.line_items.sole).to have_attributes(
+          variant: variant,
+          quantity: 1
+        )
+      end
+    end
+  end
+end
